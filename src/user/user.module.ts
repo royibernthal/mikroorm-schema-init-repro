8fe5034331d7886d33dd89adf64d0e5caf87278a
Royi Bernthal <royibernthal@gmail.com>
2025-05-09 19:25:58 +0300

    further simplify

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,9 +3,5 @@ import { MikroOrmModule } from '@mikro-orm/nestjs';
 
 import { User } from './entities';
 
-@Module({
-  imports: [MikroOrmModule.forFeature([User])],
-  providers: [],
-  exports: [MikroOrmModule.forFeature([User])],
-})
+@Module({ imports: [MikroOrmModule.forFeature([User])] })
 export class UserModule {}
